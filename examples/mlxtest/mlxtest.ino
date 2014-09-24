d96137d142460792c73b7fb9eb30cc29025920ae
Paul Kourany <pkourany@users.noreply.github.com>
2014-09-24 09:01:09 -0400

    Update mlxtest.ino

--- examples/mlxtest/mlxtest.ino
+++ examples/mlxtest/mlxtest.ino
@@ -16,8 +16,12 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
-//#include <Wire.h>
+#if defined (SPARK)
 #include "Adafruit_MLX90614.h"
+#else
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+#endif
 
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
